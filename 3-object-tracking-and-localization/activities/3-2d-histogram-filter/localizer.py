5a4f1c83c5272db92f12147bf1fda96775b8f6cf
Silvio Fragnani <silviofragnanisilva@gmail.com>
2020-05-05 23:29:33 +0200

    Added the other missing activities and projects

---
+++ 3-object-tracking-and-localization/activities/3-2d-histogram-filter/localizer.py
@@ -0,0 +1,51 @@
+# import pdb
+from helpers import normalize, blur
+
+def initialize_beliefs(grid):
+    height = len(grid)
+    width = len(grid[0])
+    area = height * width
+    belief_per_cell = 1.0 / area
+    beliefs = []
+    for i in range(height):
+        row = []
+        for j in range(width):
+            row.append(belief_per_cell)
+        beliefs.append(row)
+    return beliefs
+
+def sense(color, grid, beliefs, p_hit, p_miss):
+    new_beliefs = []
+    
+    print grid
+    
+    for i in range(len(grid)):
+        row = []
+        for j in range(len(grid[0])):
+            if grid[i][j] == color:
+                row.append(p_hit)
+            else:
+                row.append(p_miss)
+        new_beliefs.append(row)   
+    
+    sum_new_beliefs = 0
+    for i in range(len(new_beliefs)):
+        sum_new_beliefs += sum(new_beliefs[i])
+    
+    for i in range(len(new_beliefs)):
+        for j in range(len(new_beliefs[0])):
+            new_beliefs[i][j] = new_beliefs[i][j] / sum_new_beliefs
+            
+    return new_beliefs
+
+def move(dy, dx, beliefs, blurring):
+    height = len(beliefs)
+    width = len(beliefs[0])
+    new_G = [[0.0 for i in range(width)] for j in range(height)]
+    for i, row in enumerate(beliefs):
+        for j, cell in enumerate(row):
+            new_i = (i + dy) % height
+            new_j = (j + dx) % width
+            # pdb.set_trace()
+            new_G[int(new_i)][int(new_j)] = cell
+    return blur(new_G, blurring)
